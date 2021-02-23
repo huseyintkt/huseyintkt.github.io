979331c175e22243c83508130a640cccf48c0aa2
= <huseyintkt41@gmail.com>
2021-02-24 00:03:33 +0100

    updated project

---
+++ precache-manifest.b6775887212af24468b46ae793aa97fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7dbfa2a2b18ffbc13eb8c862677f5d",
+    "url": "/Pref-Documents/index.html"
+  },
+  {
+    "revision": "0ff3c3c0584f66fe432b",
+    "url": "/Pref-Documents/static/css/2.427a1f5a.chunk.css"
+  },
+  {
+    "revision": "b42dab3b5583231cd511",
+    "url": "/Pref-Documents/static/css/main.0434586e.chunk.css"
+  },
+  {
+    "revision": "0ff3c3c0584f66fe432b",
+    "url": "/Pref-Documents/static/js/2.8170d8b1.chunk.js"
+  },
+  {
+    "revision": "166b61fd4bd540afed78fd9d5c882b00",
+    "url": "/Pref-Documents/static/js/2.8170d8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42dab3b5583231cd511",
+    "url": "/Pref-Documents/static/js/main.8fdc1956.chunk.js"
+  },
+  {
+    "revision": "0b484f80b1527deddb22",
+    "url": "/Pref-Documents/static/js/runtime-main.5e782389.js"
+  },
+  {
+    "revision": "18c3888f364246a853eb25e459360731",
+    "url": "/Pref-Documents/static/media/change.18c3888f.svg"
+  }
+]);
